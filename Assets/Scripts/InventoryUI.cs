bffd094cb202ee5d6a8fea9c82c087eb7d4ff2f2
Roshan-y <115220107+Roshan-y@users.noreply.github.com>
2025-05-20 14:47:35 +0200

    feat: add inventory

--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -5,19 +5,43 @@ using System.Collections.Generic;
 public class InventoryUI: MonoBehaviour
 {
     private List<Item> itemList = new List<Item>();
+    private List<Image> slotImages;
     private int maxSlots = 5;
-    private int currentSlot = 0;
+
+    void Awake()
+    {
+        slotImages = new List<Image>();
+        for (int i = 1; i <= maxSlots; i++)
+        {
+            Transform itemTransform = transform.Find($"InventorySlot{i}/InventoryItem");
+            if (itemTransform != null)
+            {
+                Image img = itemTransform.GetComponent<Image>();
+                if (img != null)
+                    slotImages.Add(img);
+                else
+                    Debug.LogWarning($"No Image component found on InventorySlot{i}/InventoryItem");
+            }
+            else
+            {
+                Debug.LogWarning($"InventorySlot{i}/InventoryItem not found on {name}");
+            }
+        }
+    }
 
     public void AddItem(Item item)
     {
-        if (currentSlot >= maxSlots)
+        foreach (var img in slotImages)
         {
-            Debug.Log("Inventory is full!");
-            return;
+            if (img.sprite == null)
+            {
+                img.sprite = item.icon;
+                itemList.Add(item);
+                int slotIndex = slotImages.IndexOf(img) + 1;
+                Debug.Log($"Added {item.itemType} to slot {slotIndex}");
+                return;
+            }
         }
-
-        itemList.Add(item);
-        currentSlot++;
-        Debug.Log($"Added {item.name} as {item.itemType} to slot {currentSlot}");
+        Debug.Log("Inventory is full!");
     }
 }
